8c0849a7b7a765e3d2626678222ac3230d701ac7
evirson <evirson@vetorsistemas.com.br>
2022-12-30 16:07:23 -0300

    atualizado com Login ok

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,21 +12,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-
 import { LoginComponent, LogarComponent } from './components';
-
+import { LoginService } from './services';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    LogarComponent
-    
-  ],
+  declarations: [LoginComponent, LogarComponent],
   imports: [
     CommonModule,
-    RouterModule, 
+    RouterModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule,
+  ],
+  providers:[
+    LoginService
   ]
 })
-export class LoginModule { }
+export class LoginModule {}
